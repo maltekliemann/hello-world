e0f7ede07164404bfc685d314ed1dacf104307a8
maltekliemann <mail@maltekliemann.com>
2021-06-14 17:58:45 +0200

    .

--- setup.py
+++ setup.py
@@ -1,12 +1,3 @@
 from setuptools import setup
 
-setup(
-    name='hello-world',
-    author='me',
-    version='1.0.0',
-    packages=['hello'],
-    package_dir={'': 'src'},
-    scripts=['src/hello-world'],
-    include_package_data=True,
-    python_requires='>=3.7',
-)
+setup(name='hello-world', scripts=['src/hello-world'])
